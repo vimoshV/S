12aa3687557f5c79212ffb94fe1dd89ea71c2afc
Vimosh Vasanthakumar <159552821+vimoshV@users.noreply.github.com>
2024-04-17 08:53:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const groupAData = [
+    { school: 'SMC', W: 3, L: 5, PTS: 2 * 3, ZScore: 0.782 },
+    { school: 'MMC', W: 4, L: 4, PTS: 2 * 4, ZScore: 0.682 },
+    { school: 'ABC', W: 3, L: 5, PTS: 2 * 3, ZScore: 0.712 },
+    { school: 'XYZ', W: 6, L: 2, PTS: 2 * 6, ZScore: 0.312 }
+  ];
+
+  const groupBData = [
+    { school: 'SMC', W: 4, L: 4, PTS: 2 * 4, ZScore: 0.782 },
+    { school: 'MMC', W: 4, L: 4, PTS: 2 * 4, ZScore: 0.682 },
+    { school: 'ABC', W: 4, L: 4, PTS: 2 * 4, ZScore: 0.712 },
+    { school: 'XYZ', W: 6, L: 2, PTS: 2 * 6, ZScore: 0.312 }
+  ];
+
+  const sortedGroupA = groupAData.sort((a, b) => {
+    if (a.W === b.W) {
+      if (a.PTS === b.PTS) {
+        return b.ZScore - a.ZScore;
+      }
+      return b.PTS - a.PTS;
+    }
+    return b.W - a.W;
+  });
+
+  const sortedGroupB = groupBData.sort((a, b) => {
+    if (a.W === b.W) {
+      if (a.PTS === b.PTS) {
+        return b.ZScore - a.ZScore;
+      }
+      return b.PTS - a.PTS;
+    }
+    return b.W - a.W;
+  });
+
+function handleContinueClick() {
+    document.querySelector('.left-section').style.display = 'none';
+    document.querySelector('.right-section').style.display = 'block';
+
+
+}
+
+
+  window.onload = function() {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+      document.querySelector('.left-section').style.display = 'none';
+    }
+    renderTable('groupATable', sortedGroupA);
+    renderTable('groupBTable', sortedGroupB);
+  };
+
+  function renderTable(tableId, data) {
+    const tableBody = document.getElementById(tableId).getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+    data.forEach((item, index) => {
+      const row = tableBody.insertRow();
+      row.insertCell().textContent = index + 1;
+      row.insertCell().textContent = item.school;
+      row.insertCell().textContent = item.W;
+      row.insertCell().textContent = item.L;
+      row.insertCell().textContent = item.PTS;
+      row.insertCell().textContent = item.ZScore;
+    });
+  }
